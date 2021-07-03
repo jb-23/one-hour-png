649557cdcf6c21c4e2059db4afe9c706f3d67b0b
Jason Bamford <47010074+jb-23@users.noreply.github.com>
2021-07-03 13:16:43 +0000

    Add image data, scan and filter functions

--- writepng.py
+++ writepng.py
@@ -12,6 +12,16 @@ def main():
 
     print(f"header = {chunk_ihdr}\n")
 
+    red =   [   0,  85, 170, 255,     0,  85, 170, 255,     0,  85, 170, 255,     0,  85, 170, 255 ]
+    green = [   0,   0,   0,   0,    85,  85,  85,  85,   170, 170, 170, 170,   255, 255, 255, 255 ]
+    blue =  [ 255,   0,   0,   0,     0, 255,   0,   0,     0,   0, 255,   0,     0,   0,   0, 255 ]
+
+    blob = pass_image(width=4, planes=[red, green, blue])
+    data = zlib.compress(bytes(blob))
+    chunk_idat = make_chunk("IDAT", data)
+
+    print(f"data = {chunk_idat}\n")
+
 
 def make_chunk(type, data=b""):
     b = len(data).to_bytes(4, byteorder="big")  # Length of Data
@@ -31,6 +41,26 @@ def make_header(width, height):
     b += (0).to_bytes(1, byteorder="big")     # Filter Method
     b += (0).to_bytes(1, byteorder="big")     # Interlace Method = None
     return b
+
+def pass_image(width, planes):
+    lines = int(len(planes[0]) / width)
+    blob = []
+    for i in range(0, lines):
+        scanline = get_scanline(i, width, planes)
+        filtered = filter_0(scanline)
+        blob += filtered
+    return blob
+
+def get_scanline(line_number, width, planes):
+    start = line_number * width
+    scanline = []
+    for i in range(start, start+width):
+        for p in planes:
+            scanline.append(p[i])
+    return scanline
+
+def filter_0(scanline):
+    return [0] + scanline
 
 
 if __name__ == "__main__":
